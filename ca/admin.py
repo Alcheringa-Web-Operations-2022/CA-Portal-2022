ed606d438ecb2aac0ae91f37f15aa7987e3c7c20
Bhavik <84555760+TheProParadox@users.noreply.github.com>
2021-09-29 19:12:27 +0530

    Update admin.py

--- ca/admin.py
+++ ca/admin.py
@@ -8,17 +8,17 @@ class POCAdmin(admin.ModelAdmin):
 	list_display = ('user', 'name', 'design', 'college', 'contact')
 
 	def approve_poc(self, request, queryset):
-		for venue in queryset:
-			venue.approval=True
-			venue.save()
+		for poc in queryset:
+			poc.approval=True
+			poc.save()
 		
 		self.message_user(request, "All the selected PoCs have been approved successfully.")
 	approve_poc.short_description = 'Approve all the selected PoCs'
 
 	def disapprove_poc(self, request, queryset):
-		for venue in queryset:
-			venue.approval=False
-			venue.save()
+		for poc in queryset:
+			poc.approval=False
+			poc.save()
 		self.message_user(request, "All the selected PoCs have been disapproved successfully.")
 	disapprove_poc.short_description = 'Disapprove all the selected PoCs'
 
