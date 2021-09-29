83360c513697247c7cbbf38dec070e9ffc644c0c
Bhavik <84555760+TheProParadox@users.noreply.github.com>
2021-09-29 19:24:57 +0530

    Update admin.py

--- ca/admin.py
+++ ca/admin.py
@@ -6,6 +6,8 @@ import csv,io
 
 class POCAdmin(admin.ModelAdmin):
 	list_display = ('user', 'name', 'design', 'college', 'contact')
+        list_filter = ("approval",)
+	readonly_fields = ['POCscore',]
 
 	def approve_poc(self, request, queryset):
 		for poc in queryset:
@@ -21,5 +23,38 @@ class POCAdmin(admin.ModelAdmin):
 			poc.save()
 		self.message_user(request, "All the selected PoCs have been disapproved successfully.")
 	disapprove_poc.short_description = 'Disapprove all the selected PoCs'
+ 
+def save_model(self, request, obj, form, change):
+		if 'approval' in form.changed_data:
+			delta = POC_SCORE
+			if obj.pk:
+				old_value = POC.objects.get(pk=obj.pk).approval
+				if old_value == 1:
+					delta = -POC_SCORE
+				elif (old_value == -1 and obj.approval == 0) or (old_value == 0 and obj.approval == -1):
+					delta = 0
+				
+			elif obj.approval != 1:
+				delta = 0
+
+			obj.POCscore+=delta
+			obj.user.ca_details.score+=delta
+			#TriWeekly score function
+			# if  delta > 0 or obj.triweeklyPOC!=0:
+			# 	obj.triweeklyPOC+=delta
+			# 	obj.user.ca_details.triweekly+=delta
+
+		super().save_model(request, obj, form, change)
+		obj.user.ca_details.save()
+
+	def delete_model(self,request,obj):
+		if obj.POCscore == POC_SCORE:
+			obj.user.ca_details.score-=POC_SCORE
+
+		#TriWeekly function
+		# if obj.triweeklyPOC == POC_SCORE:
+		# 	obj.user.ca_details.triweekly-=POC_SCORE
+		super().delete_model(request,obj)
+		obj.user.ca_details.save()
 
 
