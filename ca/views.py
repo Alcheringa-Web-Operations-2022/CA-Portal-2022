8547a883b306bb451014ab44297a4963e18ff522
Bhavik <84555760+TheProParadox@users.noreply.github.com>
2021-09-29 19:07:24 +0530

    Update views.py

--- ca/views.py
+++ ca/views.py
@@ -40,3 +40,49 @@ def poc(request):
         context = {}
 
         return render(request,template,context)
+    
+  
+@login_required
+def poc_form(request):
+        if request.method == 'POST':
+                
+                data = {}
+                if request.POST['poc_name'] == '':
+                        data['poc_name'] = "Name Empty"
+                
+                if request.POST['poc_design'] == '':
+                        data['poc_design'] = "Designation Empty"
+
+                if request.POST['poc_college'] == '':
+                        data['poc_college'] = "College Empty"
+
+                if request.POST['poc_phone'] == '':
+                                         data['poc_phone'] = "Phone Empty"
+                else:
+                        contact_number_validator = RegexValidator('^[0-9]{10}$')
+                        try:
+                                contact_number_validator(request.POST['poc_phone'])
+                        except Exception as e:
+                                data['poc_phone'] = "Phone REGEX"
+                if data:
+                        data['stat'] = "FAILURE"
+                        return JsonResponse(data)
+                else :
+                        poc_name = request.POST['poc_name']
+                        poc_design= request.POST['poc_design']
+                        poc_college = request.POST['poc_college']
+                        poc_phone = request.POST['poc_phone']
+
+                        POC.objects.create(user=request.user,name=poc_name, design=poc_design,
+                                clg=poc_college, contact=poc_phone)
+                        data['stat'] = "Success"
+
+                        return JsonResponse(data)
+        else:
+                
+                poc_queries = POC.objects.filter(user=request.user)
+                context = {
+                        'more_active': True,
+                        'poc_queries': poc_queries
+                }
+                return render(request, 'ca/poc.html', context)
