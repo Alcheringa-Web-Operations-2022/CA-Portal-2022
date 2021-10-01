ee1af8abf0f395e26eae15b401b4bca20396823f
Bhavik <84555760+TheProParadox@users.noreply.github.com>
2021-10-01 20:47:41 +0530

    Update forms.py

--- ca/forms.py
+++ ca/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import POCBulkUpload
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -7,10 +7,4 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
-class POCBulkUploadForm(forms.ModelForm):
-  class Meta:
-    model = POCBulkUpload
-    fields = ("csv_file",)
-
 
